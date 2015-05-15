38a1887b263b9f54b218c7165d79c32cb8252aef
DrVonTrap <DrVonTrap@gmail.com>
2015-05-15 14:17:37 +0100

    yo

--- update.py
+++ update.py
@@ -26,7 +26,7 @@ def gatherNewRedditURLs():
 	for subReddit_ in subReddits:
 		subReddit = subReddit_[0]
 		print("getting urls for", subReddit)
-		urlList = reddit.getHot(subReddit, 50) + reddit.getTopAll(subReddit, 100) + reddit.getTopMonth(subReddit, 500)
+		urlList = reddit.getHot(subReddit, 50) + reddit.getTopMonth(subReddit, 100)
 		for url in urlList:
 			database.addURL(url, subReddit_)
 
